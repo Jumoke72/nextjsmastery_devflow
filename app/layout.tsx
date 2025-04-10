96ae5dd4ee834fe2b20d5f7cd4bdea370ecaf32a
jumoke72 <daramolajumoke@yahoo.com>
2025-04-10 02:39:46 +0100

    Navbar commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,20 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/context/Theme";
+import Navbar from "@/components/navigation/navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const inter = localFont({
+  src:"./fonts/inter-VF.ttf",
+  variable: "--font-inter",
+  weight:"100 200 300 400 500 600 700 800 900"
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const spaceGrotesk = localFont({
+  src: "./fonts/SpaceGrotesk-VF.ttf",
+  variable: "--font-space-grotesk",
+  weight: "300 400 500 600 700",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
         <ThemeProvider
           attribute="class"
@@ -34,7 +38,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {" "}
+          <Navbar />
           {children}
         </ThemeProvider>
       </body>
